8c1f28861c5f43e8dd5018f6163cf2a827b546e0
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-10-23 16:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const timer=document.getElementById("timer");
+
+const date=new Date();
+
+setInterval(()=>{
+        const currentTime=new Date();
+		timer.innerText=currentTime.toLocaleDateString()+", "+currentTime.toLocaleTimeString();
+},1000);
